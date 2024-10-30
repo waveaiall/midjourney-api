25d7e176995c7e9c248c2a5356dbcb06ccb0f15f
derongtang.sam <derongtang.sam@bytedance.com>
2024-10-30 14:15:49 +0800

    [feat]: schedule the refresh token from db

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,12 +1,19 @@
 __version__ = '0.0.1'
 
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from asyncio_throttle import Throttler
+from loguru import logger
 
 from mysql import token_rate_limit
 
 TOKEN_2_LIMIT = {auth_token.token: Throttler(rate_limit=auth_token.rateLimit, period=60) for auth_token in token_rate_limit.selectAllEffective()}
+scheduler = BackgroundScheduler()
 
+def refreshToken():
+     global TOKEN_2_LIMIT 
+     TOKEN_2_LIMIT = {auth_token.token: Throttler(rate_limit=auth_token.rateLimit, period=60) for auth_token in token_rate_limit.selectAllEffective()}
+     logger.info(f'refresh token info {TOKEN_2_LIMIT.keys()} limit {TOKEN_2_LIMIT.values()}')
 
 def isValid(token:str):
     return token and token in TOKEN_2_LIMIT
@@ -14,3 +21,8 @@ def isValid(token:str):
 
 def getThrottler(token:str):
     return TOKEN_2_LIMIT[token]
+
+# 示例 4: 每天的每隔 5 分钟执行一次
+scheduler.add_job(refreshToken, 'cron', minute='*/5')
+
+scheduler.start()
